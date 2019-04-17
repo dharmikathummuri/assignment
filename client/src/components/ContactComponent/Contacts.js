b53e7ba248cfb064a83a412e433eb8ed5e3d5790
dharmika <dharmikasharma@gmail.com>
2019-04-17 17:28:57 -0400

    get all contacts from api and dispaly in a table ,save contacts data in redux

--- client/src/components/ContactComponent/Contacts.js
+++ client/src/components/ContactComponent/Contacts.js
@@ -1,61 +1,66 @@
 import React, { Component } from "react";
 import "./Contacts.scss";
+import { connect } from "react-redux";
+import { allContacts } from "../../actions/contactsActions";
+import { getContacts } from "../../contactsApiEndPoint";
 class Contacts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { contacts: "" };
+    this.getContactsFromApi();
+  }
+  async getContactsFromApi() {
+    let result = await getContacts();
+    this.setState({ contacts: result });
+    await this.props.allContacts(result);
+  }
+  renderContactList() {
+    const { contacts } = this.state;
+    let rows = [];
+    for (let i = 0; i < contacts.length; i++) {
+      rows.push(
+        <tr key={contacts[i]["id"]}>
+          <td>{contacts[i]["name"]} </td>
+          <td> {contacts[i]["email"]} </td>
+          <td> {contacts[i]["phone"]} </td>
+          <td>
+            <button>Update</button>
+          </td>
+          <td>
+            <button>Delete</button>
+          </td>
+        </tr>
+      );
+    }
+    return rows;
+  }
   render() {
-    const contacts = [
-      {
-        id: "1",
-        name: "Vergie -    Vankirk",
-        email: "Vergie.Vankirk@gmailio.com",
-        phone: "374 684-683"
-      },
-      {
-        id: "2",
-        name: "Milford^Mizzell",
-        email: "milfordmizzell@gmailio.com",
-        phone: "5052812342"
-      },
-      {
-        id: "3",
-        name: "Magen Mart",
-        email: "magen.mart@eggmail.com",
-        phone: "(757) 702-8058"
-      }
-    ];
-    const conatctList = contacts.map(contact => (
-      <tr>
-        <td>{contact.name} </td>
-        <td> {contact.email} </td>
-        <td> {contact.phone} </td>
-        <td>
-          <button>Update</button>
-        </td>
-        <td>
-          <button>Delete</button>
-        </td>
-      </tr>
-    ));
+    const contacts = this.state.contacts;
     return (
-      <div className="contact-class">
-        <div className="contacts-header">
-          <h1> All contacts</h1>
+      contacts && (
+        <div className="contact-class">
+          <div className="contacts-header">
+            <h1> All contacts</h1>
+          </div>
+          <div className="table-class">
+            <table>
+              <tbody>
+                <tr>
+                  <th>Full Name</th>
+                  <th>Email</th>
+                  <th> Phone</th>
+                </tr>
+                {this.renderContactList()}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="table-class">
-          <table>
-            <tbody>
-              <tr>
-                <th>Full Name</th>
-                <th>Email</th>
-                <th> Phone</th>
-              </tr>
-              {conatctList}
-            </tbody>
-          </table>
-        </div>
-        {/* <ul>{peopleLis}</ul> */}
-      </div>
+      )
     );
   }
 }
 
-export default Contacts;
+export default connect(
+  null,
+  { allContacts }
+)(Contacts);
--- client/src/contactsApiEndPoint.js
+++ client/src/contactsApiEndPoint.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const getContacts = async () => {
   let allContacts = await axios.get("http://localhost:4000/contacts");
-  return allContacts.data;
+  return allContacts.data.contacts;
 };
